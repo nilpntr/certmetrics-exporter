33a4e2396edeb2d963ae4e27eb47b12de3fd0503
Sam Mobach <s.mobach@marktmonitor.com>
2024-10-21 12:47:50 +0200

    breaking change: first release

--- .releaserc.js
+++ .releaserc.js
@@ -1,23 +1,108 @@
 module.exports = {
-    "branches": [
-        {
-            "name": "develop",
-            "prerelease": true
-        },
-        {
-            "name": "main",
-            "prerelease": false
-        }
-    ],
+    "branches": ["main"],
     "plugins": [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
         [
-            "@semantic-release/git",
+            "@semantic-release/commit-analyzer",
             {
-                "assets": [
-                    "CHANGELOG.md"
+                "preset": "angular",
+                "parserOpts": {
+                    "noteKeywords": [
+                        "BREAKING CHANGE",
+                        "BREAKING CHANGES",
+                        "BREAKING"
+                    ]
+                },
+                "releaseRules": [
+                    {
+                        "type": "build",
+                        "release": "patch"
+                    },
+                    {
+                        "type": "deploy",
+                        "release": "patch"
+                    },
+                    {
+                        "type": "chore",
+                        "release": "patch"
+                    },
+                    {
+                        "type": "docs",
+                        "release": "patch"
+                    },
+                    {
+                        "type": "test",
+                        "release": "patch"
+                    },
+                    {
+                        "type": "style",
+                        "release": "patch"
+                    },
+                    {
+                        "type": "ci",
+                        "release": "patch"
+                    }
                 ]
+            }
+        ],
+        [
+            "@semantic-release/release-notes-generator",
+            {
+                "preset": "conventionalcommits",
+                "parserOpts": {
+                    "noteKeywords": [
+                        "BREAKING CHANGE",
+                        "BREAKING CHANGES",
+                        "BREAKING"
+                    ]
+                },
+                "writerOpts": {
+                    "commitsSort": [
+                        "subject",
+                        "scope"
+                    ]
+                },
+                "presetConfig": {
+                    "types": [
+                        {
+                            "type": "feat",
+                            "section": "Features"
+                        },
+                        {
+                            "type": "fix",
+                            "section": "Bug Fixes"
+                        },
+                        {
+                            "type": "chore",
+                            "section": "Chores",
+                            "hidden": false
+                        },
+                        {
+                            "type": "refactor",
+                            "section": "Internal",
+                            "hidden": false
+                        },
+                        {
+                            "type": "perf",
+                            "section": "Performance",
+                            "hidden": false
+                        },
+                        {
+                            "type": "docs",
+                            "section": "Documentation",
+                            "hidden": false
+                        },
+                        {
+                            "type": "ci",
+                            "section": "DevOps",
+                            "hidden": false
+                        },
+                        {
+                            "type": "test",
+                            "section": "Tests",
+                            "hidden": false
+                        }
+                    ]
+                }
             }
         ],
         "@semantic-release/github"
